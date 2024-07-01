7879e5e695a7b92f03e663ea150204d68df97490
Micael-Rodrigues <micaelvitor709@gmail.com>
2024-07-01 02:03:50 -0300

    fetch: Modal

--- app/main.py
+++ app/main.py
@@ -2,14 +2,14 @@ import flet as ft
 import app.controllers.file_treatment as ftm
 
 def main(page: ft.Page):
-    initial_width = 450
-    initial_height = 550
+    initial_width = 550
+    initial_height = 600
     page.window_width = initial_width
     page.window_height = 600
     page.title = 'CleanSheets'
     page.window_always_on_top = True
-    page.window_resizable = False
     page.scroll = 'always'
+    page.horizontal_alignment = 'center'
     
     title = ft.Text(
         size=25,
@@ -25,34 +25,24 @@ def main(page: ft.Page):
         value='Selecione o arquivo',
         size=20,
     )
-
-        # main_window_width = page.window_width
-        # main_window_height = page.window_height
-        
-    # def modal(e):
-    #     modal_window = ft.AlertDialog(
-    #         [
-    #             ft.Text(value="Janela Modal", size=20),
-    #             ft.Text(value="Esta janela deve ser fechada para interagir com a de fundo."),
-    #             ft.ElevatedButton(
-    #                 text="Fechar Janela Modal",
-    #                 on_click=lambda _: page.remove(modal_window),
-    #             ),
-    #         ],
-    #         alignment=ft.MainAxisAlignment.CENTER,
-    #         expand=1,
-    #     )
     
     def handle_close(e):
         page.add(ft.Text("Modal fechado"))
+        page.close(dlg_modal)
         
     dlg_modal = ft.AlertDialog(
         modal=True,
         title=ft.Text("Planilhas Disponíveis"),
         actions=[
-            ft.TextButton("Fechar", on_click=lambda _: page.close(dlg_modal)),
+            ft.Row(
+                controls=[
+                    ft.TextButton("cancelar", on_click=lambda _: page.close(dlg_modal)),
+                    ft.TextButton("Seguir", on_click=handle_close),
+                ],
+                width=initial_width,
+                alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+            )
         ],
-        on_dismiss=handle_close,
     )
     
     def on_result(e: ft.FilePickerResultEvent):
@@ -62,7 +52,6 @@ def main(page: ft.Page):
             file_name_value = e.files[0].name
             file_path = e.files[0].path
             ext = file_name_value.split('.')[-1].lower()
-            # result_text.value = f"Arquivo selecionado: {file_name_value}\nCaminho: {file_path}"
             result_text.value = f"Caminho: {file_path}"
 
             if ext in SUPPORTED_EXTENSIONS:
@@ -70,23 +59,41 @@ def main(page: ft.Page):
                 
                 print(f'sheet:   {sheetnames}')
                 
-                modal_content = ft.Column(
-                    controls=[
-                        ft.Text(value="Planilhas encontradas:", size=18),
-                        *[ft.Text(value=f"- {sheet}") for sheet in sheetnames],
-                    ],
+                radio_group = ft.RadioGroup(
+                    content=ft.Column(
+                        controls=[
+                            ft.Container(
+                                content=ft.Radio(
+                                    value=sheet,
+                                    label=f'{sheet}',
+                                ),
+                                border=ft.border.all(1, ft.colors.WHITE10),
+                                border_radius=ft.border_radius.all(7),
+                                padding=ft.padding.all(5),
+                            )
+                            for sheet in sheetnames
+                        ],
+                        spacing=10,
+                        expand=True,
+                    )
                 )
-                dlg_modal.content = modal_content
                 
-                page.open(dlg_modal)
-                                
+                # modal_content = ft.Container(
+                #     content=radio_group,
+                #     border=ft.border.all(1, ft.colors.BLACK),
+                #     expand=False,
+                # )
+                
+                dlg_modal.content = radio_group
+                
+                page.dialog = dlg_modal
+                dlg_modal.open = True
+                
                 num_cols = columns if columns != 0 else 0
                 
-                file_info.value = f"Arquivo: {file_name_value}"
+                file_name.value = f"Arquivo: {file_name_value}"
 
                 if data_file is not None:
-                    # result_title.value = 'Resultado:'
-                    # result_text.value = f'Dados processados: {data_file}'
                     result_columns.value = f'Número de colunas: {num_cols}'
                 else:
                     result_title.value = 'Erro:'
@@ -100,7 +107,7 @@ def main(page: ft.Page):
             page.update()
 
     file_picker = ft.FilePicker(on_result=on_result)
-    file_info = ft.Text()
+    file_name = ft.Text()
     result_title = ft.Text(size=16)
     result_text = ft.Text()
     result_columns = ft.Text()
@@ -121,17 +128,18 @@ def main(page: ft.Page):
             checkbox = ft.Checkbox(
                 label=f'{i+1} - {column_names[i]}' if i < len(column_names) else f'None {i+1}',
                 value=False,
-                width=100,
+                width=130,
                 height=30,
             )
             checkbox_container = ft.Container(
                 content=checkbox,
                 padding=ft.padding.all(5),
-                border=ft.border.all(1, ft.colors.BLACK),
+                border=ft.border.all(1, ft.colors.WHITE10),
+                border_radius=ft.border_radius.all(7),
             )
             checkboxes.append(checkbox_container)
         checkboxes_row.controls = checkboxes
-
+        
     checkboxes_row = ft.Row(
         alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
         width=initial_width,
@@ -145,15 +153,15 @@ def main(page: ft.Page):
             controls=[
                 ft.Row(
                     controls=[
-                        file_info,
+                        file_name,
                         result_columns,
                     ],
                     spacing=10,
                     alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
                 ),
                 checkboxes_row,
-                result_title,
                 result_text,
+                result_title,
             ],
         )
     )
