4e5ce3260d1c3fa4a84d9fee9a8b2c1a64572774
Micael-Rodrigues <micaelvitor709@gmail.com>
2024-07-02 21:31:54 -0300

    box for selected

--- app/controllers/file_treatment.py
+++ app/controllers/file_treatment.py
@@ -2,8 +2,6 @@ import flet as ft
 from openpyxl import load_workbook
 
 def file_treatment(file):
-    title_row = 1
-
     try:
         wb = load_workbook(file.path)
         sheet = wb.active
@@ -15,20 +13,14 @@ def file_treatment(file):
                         return cell.value, cell.row
 
         row_with_value = define_row_with_value()
-        cell_value = row_with_value[0]
         cell_row = row_with_value[1]
-
-        print(f'row with value: {cell_row}')
 
         title_row_values = sheet[cell_row]
         column_names = [cell.value for cell in title_row_values]
 
-        num_cols = sheet.max_column
-        print(f'Número de colunas na planilha: {num_cols}')
-
         wb.close()
 
-        return column_names, num_cols, wb.sheetnames
+        return column_names, wb.sheetnames
     except Exception as e:
         print(f"Erro ao processar arquivo: {e}")
         return None, None
@@ -45,10 +37,7 @@ def get_columns_from_sheet(file, sheet_name):
                         return cell.value, cell.row
 
         row_with_value = define_row_with_value()
-        cell_value = row_with_value[0]
         cell_row = row_with_value[1]
-
-        print(f'row with value: {cell_row}')
 
         title_row_values = sheet[cell_row]
         column_names = [cell.value for cell in title_row_values]
--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
 import flet as ft
 import app.controllers.file_treatment as ftm
+import app.controllers.delete_file as delf
 
 def main(page: ft.Page):
     initial_width = 550
     initial_height = 600
-    page.window_width = initial_width
-    page.window_height = initial_height
+    page.window.width = initial_width
+    page.window.height = initial_height
+    page.window.always_on_top = True
     page.title = 'CleanSheets'
-    page.window_always_on_top = True
     page.scroll = 'always'
     page.horizontal_alignment = 'center'
     
@@ -26,16 +27,49 @@ def main(page: ft.Page):
         size=20,
     )
     
-    selected_sheet = None
-    selected_file = None  # Variável global para armazenar o arquivo selecionado
+    checkboxes_row = ft.Row(
+        alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+        width=initial_width,
+        spacing=10,
+        wrap=True,
+    )
+    
+    checkboxes = []
 
+    def inputs_create(num_checkboxes, column_names):
+        print(f'column_names: {column_names}')
+        checkboxes.clear()
+        for i in range(num_checkboxes):
+            checkbox = ft.Checkbox(
+                label=f'{i+1} - {column_names[i]}' if i < len(column_names) else f'None {i+1}',
+                value=False,
+                width=130,
+                height=30,
+            )
+            checkbox_container = ft.Container(
+                content=checkbox,
+                padding=ft.padding.all(5),
+                border=ft.border.all(1, ft.colors.WHITE10),
+                border_radius=ft.border_radius.all(7),
+            )
+            checkboxes.append(checkbox_container)
+        checkboxes_row.controls = checkboxes
+        page.update()
+        print('inputs created')
+    
+    selected_sheet = None
+    selected_file = None
+    
     def handle_close(e):
         if selected_sheet and selected_file:
             column_names = ftm.get_columns_from_sheet(selected_file, selected_sheet)
             inputs_create(len(column_names), column_names)
+            
+            result_columns.value = f'Número de colunas: {len(column_names)}'
+            
             dlg_modal.open = False
             page.update()
-        page.add(ft.Text("Modal fechado"))
+            
         page.close(dlg_modal)
         
     dlg_modal = ft.AlertDialog(
@@ -44,7 +78,7 @@ def main(page: ft.Page):
         actions=[
             ft.Row(
                 controls=[
-                    ft.TextButton("cancelar", on_click=lambda _: page.close(dlg_modal)),
+                    ft.TextButton("Cancelar", on_click=lambda _: page.close(dlg_modal)),
                     ft.TextButton("Seguir", on_click=handle_close),
                 ],
                 width=initial_width,
@@ -62,47 +96,68 @@ def main(page: ft.Page):
             file_name_value = selected_file.name
             file_path = selected_file.path
             ext = file_name_value.split('.')[-1].lower()
-            file_path_text.value = f"Caminho: {file_path}"
 
             if ext in SUPPORTED_EXTENSIONS:
-                data_file, columns, sheetnames = ftm.file_treatment(selected_file)
+                data_file, sheetnames = ftm.file_treatment(selected_file)
                 
                 print(f'sheet: {sheetnames}')
 
-                def on_radio_selected(ev):
+                def on_container_click(index):
+                    page.update()
+                    
                     nonlocal selected_sheet
-                    selected_sheet = ev.control.value
+                    selected_sheet = sheetnames[index]
 
-                radio_group = ft.RadioGroup(
-                    content=ft.Column(
-                        controls=[
-                            ft.Container(
-                                content=ft.Radio(
-                                    value=sheet,
-                                    label=sheet,
+                    for i, container in enumerate(radio_containers):
+                        if i == index:
+                            container.bgcolor = ft.colors.WHITE10
+                        else:
+                            container.bgcolor = ft.colors.TRANSPARENT
+                        print(f'clicked:  {index} / {i} / {container}')
+                        
+                    page.update()
+
+                radio_containers = [
+                    ft.Container(
+                        content=ft.Row(
+                            controls=[
+                                ft.Text(
+                                    sheet,
+                                    size=14,
                                 ),
-                                border=ft.border.all(1, ft.colors.WHITE10),
-                                border_radius=ft.border_radius.all(7),
-                                padding=ft.padding.all(5),
-                            )
-                            for sheet in sheetnames
-                        ],
-                        spacing=10,
-                        expand=True,
-                    ),
-                    on_change=on_radio_selected
+                                ft.Text(
+                                    value=f"Colunas: {len(ftm.get_columns_from_sheet(selected_file, sheet))}",
+                                    size=11,
+                                )
+                            ],
+                            spacing=5,
+                            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+                        ),
+                        border=ft.border.all(1, ft.colors.WHITE10),
+                        border_radius=ft.border_radius.all(7),
+                        padding=ft.padding.all(10),
+                        width=initial_width,
+                        on_click=lambda _, index=i: on_container_click(index),
+                    )
+                    for i, sheet in enumerate(sheetnames)
+                ]
+                
+                radio_group = ft.Column(
+                    controls=radio_containers,
+                    spacing=10,
+                    width=initial_width,
+                    expand=True,
                 )
                 
                 dlg_modal.content = radio_group
                 
-                page.dialog = dlg_modal
+                page.overlay.append(dlg_modal)
                 dlg_modal.open = True
                 
+                file_path_text.value = f"Caminho: {file_path}"
                 file_name.value = f"Arquivo: {file_name_value}"
 
-                if data_file is not None:
-                    result_columns.value = f'Número de colunas: {columns}'
-                else:
+                if data_file is None:
                     result_title.value = 'Erro:'
                     file_path_text.value = 'Erro ao processar arquivo.'
             else:
@@ -112,44 +167,18 @@ def main(page: ft.Page):
             page.update()
 
     file_picker = ft.FilePicker(on_result=on_result)
+    page.overlay.append(file_picker)
+    
     file_name = ft.Text()
+    result_columns = ft.Text()
+    
     result_title = ft.Text(size=16)
     file_path_text = ft.Text()
-    result_columns = ft.Text()
-
-    page.overlay.append(file_picker)
 
     pick_file_button = ft.ElevatedButton(
         on_click=lambda _: file_picker.pick_files(),
         width=initial_width,
         text="Upload",
-    )
-
-    checkboxes = []
-
-    def inputs_create(num_checkboxes, column_names):
-        checkboxes.clear()
-        for i in range(num_checkboxes):
-            checkbox = ft.Checkbox(
-                label=f'{i+1} - {column_names[i]}' if i < len(column_names) else f'None {i+1}',
-                value=False,
-                width=130,
-                height=30,
-            )
-            checkbox_container = ft.Container(
-                content=checkbox,
-                padding=ft.padding.all(5),
-                border=ft.border.all(1, ft.colors.WHITE10),
-                border_radius=ft.border_radius.all(7),
-            )
-            checkboxes.append(checkbox_container)
-        checkboxes_row.controls = checkboxes
-        
-    checkboxes_row = ft.Row(
-        alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
-        width=initial_width,
-        spacing=10,
-        wrap=True,
     )
 
     result_container = ft.Container(
@@ -164,12 +193,12 @@ def main(page: ft.Page):
                     spacing=10,
                     alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
                     wrap=True,
-                    expand=True,
+                    width=initial_width,
                 ),
                 checkboxes_row,
                 file_path_text,
                 result_title,
-            ],
+            ]
         )
     )
 
@@ -187,10 +216,12 @@ def main(page: ft.Page):
             ],
         ),
     )
+    
+    page.update()
 
     page.add(
         display,
-        wrapper
+        wrapper,
     )
 
     page.update()
